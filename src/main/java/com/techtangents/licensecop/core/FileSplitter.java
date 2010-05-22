57edfefdd20790fc12e0f67d81655cae86db8e31
techtangents <ddoctor@samplesurgeon.com>
2010-05-23 01:29:55 +1000

    Added apache 2 license headers. I hate them.

--- src/main/java/com/techtangents/licensecop/core/FileSplitter.java
+++ src/main/java/com/techtangents/licensecop/core/FileSplitter.java
@@ -1,3 +1,19 @@
+/*
+Copyright 2010 Dylan Just
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package com.techtangents.licensecop.core;
 
 import com.ephox.epipes.core.Mapper;
@@ -20,7 +36,7 @@ public class FileSplitter extends Mapper<FileAndContents, FileInfo> {
         Matcher m = p.matcher(trimmed);
         if (m.matches() && m.groupCount() == 2) {
             String header = m.group(1).trim();
-            String body = m.group(2).trim();
+            String body = m.group(2).trim() + "\n";
             return new FileInfo(header, body, file);
         } else {
             return new FileInfo("", trimmed, file);
